0f54d12c7bad73f511de22bb59e395049f65deda
Kirsten Smith <kirstenalexis7@gmail.com>
2023-11-04 17:34:59 -0700

    WIP add results

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,22 @@ window.addEventListener("load", function () {
 function surveyAnswers(e) {
   e.preventDefault();
   const typed = document.querySelector('input[name="typed"]:checked').value;
+  const interest = document.querySelector('input[name="interest"]:checked').value;
+  const mobile = document.querySelector('input[name="mobile"]:checked').value;
+  const experience = document.querySelector('input[name="experience"]:checked').value;
+  const muffin = document.querySelector('input[name="muffin"]:checked').value;
 
   let results;
-  if (typed === "statically") {
-    results = "C#"
+  if (typed === "dynamic" && interest === "front" && mobile === "yes" || typed === "dynamic" && interest === "front" && mobile === "no") {
+    results = "JavaScript"
   }
+  else if (typed === "dynamic" && interest === "data" && mobile === "no") {
+    results = "Python"
+  }
+  else if (typed = "statically") {
+    results = "C#(C Sharp)"
+  }
+
 
   document.getElementById("output").innerText = results
 }
